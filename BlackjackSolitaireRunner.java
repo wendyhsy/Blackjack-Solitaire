47425a137c5f75a6e74651bed1aaa1569dd2cc94
Shengyun Wendy Huang <shengyunh@gmail.com>
2022-08-30 22:22:37 -0400

    Update BlackjackSolitaireRunner.java

---
+++ BlackjackSolitaireRunner.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author wendyhsy
+ *
+ */
+public class BlackjackSolitaireRunner {
+	
+	public static void main(String[] args) {
+		BlackjackSolitaire bjs = new BlackjackSolitaire();
+		bjs.play();
+	}
+}
